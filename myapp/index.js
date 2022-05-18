8c5427f06748f90091fcef097f9d5fc5f569297f
ks677c <ks677c@att.com>
2022-05-18 14:55:10 +0530

    test89

--- myapp/index.js
+++ myapp/index.js
@@ -1,6 +1,30 @@
 const express = require('express');
+const mysql = require('mysql2')
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database:"sakila"
+})
+
+
+connection.connect(function(err) {
+  if (err){ throw error}
+  console.log('You are connected')
+
+  connection.query("SELECT * FROM actor", function (err, result) {
+    // if any error while executing above query, throw error
+    if (err) throw err;
+    // if there is no error, you have the result
+    console.log(result);
+  });
+  
+})
 
 const app=express();
+
+let _dirname="H:\\Desktop\\Node js\\Calculator\\myapp"
 
 app.get("/date",(request,response)=>{
 
@@ -11,12 +35,11 @@ app.get("/date",(request,response)=>{
 
 });
 
-let _dirname="H:\\Desktop\\Node js\\Calculator\\myapp"
 app.get("/page",(request,response)=>{
 
     let date=new Date();
-    response.sendFile('./page.html',{ root: _dirname });
+    response.sendFile("./page.html",{ root: _dirname });
 
 });
 
-app.listen(3000)
+app.listen(3300)
